4a63108cbb0df27a252c3196939cd0f1fc238402
Rodrigo Rutkoski Rodrigues <rutkoski@gmail.com>
2015-12-10 10:30:25 -0200

    Default template extension is html.

--- lib/templates/form_element_datetime.php
+++
@@ -1,9 +0,0 @@
-<input id="{{ id }}" type="text" name="{{ inputName }}" value="{{ formatedValue }}" class="form-control" {{ disabled ? 'disabled' : '' }}/>
-
-<script>
-$(document).ready(function() {
-	$('input#{{ id }}').datetimepicker({
-		locale: 'pt-br'
-	});
-});
-</script>
